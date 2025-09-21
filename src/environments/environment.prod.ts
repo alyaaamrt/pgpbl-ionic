6d62c148f03566ad497dba1033ed64ad89450f43
alyaaamrt <alyamiranti46@gmail.com>
2025-09-21 19:35:57 +0700

    Acara 4 Ionic Firebase Authentication

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  firebaseConfig: {
+    apiKey: "AIzaSyDUUuvrwaX6zcsodVnTFGTc6JKw8FnDSVc",
+    authDomain: "pgpbl-ionic-421fa.firebaseapp.com",
+    projectId: "pgpbl-ionic-421fa",
+    storageBucket: "pgpbl-ionic-421fa.firebasestorage.app",
+    messagingSenderId: "542523811099",
+    appId: "1:542523811099:web:9e69cb577fb98e86269fbe",
+    measurementId: "G-S89LKB0VVJ"
+  }
 };
