273878de5cb7656723206cda4a8b36d29277c3dc
alyaaamrt <alyamiranti46@gmail.com>
2025-09-27 23:58:35 +0700

    Acara 5 Ionic Firebase Leaflet Point CRUD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,14 +9,23 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./splashscreen/splashscreen.module').then( m => m.SplashscreenPageModule)
-  },  {
+  },
+  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
   {
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'maps',
+    loadChildren: () => import('./maps/maps.module').then( m => m.MapsPageModule)
+  },  {
+    path: 'createpoint',
+    loadChildren: () => import('./createpoint/createpoint.module').then( m => m.CreatepointPageModule)
   }
+
 
 
 ];
